ca16c4dc8562d04e1c10407d7123432794f0a26b
Maxime Billaud <maximebio1@gmail.com>
2025-04-23 09:04:03 +0200

    ajouté un exemple

--- src/scenario_A.ts
+++ src/scenario_A.ts
@@ -33,3 +33,12 @@ class ParametreJeu {
       return ParametreJeu.instance;
     }
   }
+
+  const settings1 = ParametreJeu.getInstance();
+settings1.volumeMusique = 0.8;
+settings1.langueActuelle = "Anglais";
+
+const settings2 = ParametreJeu.getInstance();
+console.log("Langue :", settings2.langueActuelle); 
+console.log("Volume Musique :", settings2.volumeMusique); 
+console.log("Même instance ?", settings1 === settings2); 
